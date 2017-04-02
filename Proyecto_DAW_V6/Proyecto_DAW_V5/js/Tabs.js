93fbf39c7974a872503b306fa3c032be15c65e1e
erickxcs <erick.xcs@hotmail.com>
2017-04-02 17:22:55 -0600

    Delete

--- Proyecto_DAW_V6/Proyecto_DAW_V5/js/Tabs.js
+++
@@ -1,9 +0,0 @@
-$(function (activar_pestanya) {
-   				var tabContainerssup = $('div.contenedor > div');
-    
-			    $('div.tab a').click(function () {
-		        	tabContainerssup.hide().filter(this.hash).show();
-        
-		        	return false;
-			    }).filter(':first').click();
-			});
